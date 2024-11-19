5dcadc2ae13f79d369e6fba3f5669e47b8100957
Eliezer Azevedo <eliezer.system@gmail.com>
2024-11-19 11:50:26 -0300

    update

--- impressao_zpl.py
+++ impressao_zpl.py
@@ -34,34 +34,53 @@ def imprimir_zpl(zpl_content):
             win32print.ClosePrinter(handle_impressora)
 
 # Gera o código aleatório para o Code 128
-conteudo_pagina2 = gerar_code128()
+conteudo_code128 = gerar_code128()
 
 # Substitui as variáveis do ZPL com os conteúdos desejados
 zpl_code = f"""
 ^XA
-^FWB                     // Define a orientação para horizontal
-^PW800                   // Largura (10 cm convertidos para dots, supondo 203 dpi)
-^LL440                   // Altura (5.5 cm convertidos para dots, supondo 203 dpi)
-^FX Primeira Página
-^FO50,50                 // Posição do texto
-^A0N,50,50               // Fonte com altura e largura ajustadas
-^FDPrimeira Página: produto teste^FS
-^XZ
 
+^FX Top section with logo, name and address.
+^CF0,60
+^FO50,50^GB100,100,100^FS
+^FO75,75^FR^GB100,100,100^FS
+^FO93,93^GB40,40,40^FS
+^FO220,50^FDIntershipping, Inc.^FS
+^CF0,30
+^FO220,115^FD1000 Shipping Lane^FS
+^FO220,155^FDShelbyville TN 38102^FS
+^FO220,195^FDUnited States (USA)^FS
+^FO50,250^GB700,3,3^FS
+
+^FX Second section with recipient address and permit information.
+^CFA,30
+^FO50,300^FDJohn Doe^FS
+^FO50,340^FD100 Main Street^FS
+^FO50,380^FDSpringfield TN 39021^FS
+^FO50,420^FDUnited States (USA)^FS
+^CFA,15
+^FO600,300^GB150,150,3^FS
+^FO638,340^FDPermit^FS
+^FO638,390^FD123456^FS
+^FO50,500^GB700,3,3^FS
+
+^XZ
 ^XA
-^FWB                     // Define a orientação para horizontal
-^PW800                   // Largura (10 cm convertidos para dots, supondo 203 dpi)
-^LL440                   // Altura (5.5 cm convertidos para dots, supondo 203 dpi)
-^FX Segunda Página
-^FO50,50
-^A0N,50,50
-^FDSerial:^FS
-^FO50,100
-^A0N,50,50
-^FD{conteudo_pagina2}^FS
-^FO50,150
-^BCR,100,Y,N,N          // Código de barras Code128
-^FD>: {conteudo_pagina2}^FS
+
+^FX Third section with bar code.
+^BY5,2,270
+^FO100,550^BC^FD{conteudo_code128}^FS
+
+^FX Fourth section (the two boxes on the bottom).
+^FO50,900^GB700,250,3^FS
+^FO400,900^GB3,250,3^FS
+^CF0,40
+^FO100,960^FDCtr. X34B-1^FS
+^FO100,1010^FDREF1 F00B47^FS
+^FO100,1060^FDREF2 BL4H8^FS
+^CF0,190
+^FO470,955^FDCA^FS
+
 ^XZ
 """
 
